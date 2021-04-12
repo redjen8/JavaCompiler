21b91a4377cc149620a15928226c26e597ec9608
redjen8 <seokwoo2525@gmail.com>
2021-04-12 19:56:38 +0900

    test files moved

---
+++ tests/test4.java
@@ -0,0 +1,21 @@
+public class test{
+    public static void main(String[] args){
+        int a_a_a;
+        char bbb[5];
+        boolean c0c3;
+        String dd_d_1;
+
+        a_a_a -= -1--2-(-3--4)-5-6(-7--8)--9;
+        bbb[1] = 'A', c0c3 = true;
+        if(c0c3 = true){
+            dd_d_1 = "Compiler";
+        }else if(c0c3 = false){
+            dd_d_1 = "Project";
+        }
+        while(a_a_a < 100){
+            a += a * a - a / a;
+        }
+
+        return 0;
+    }
+}
